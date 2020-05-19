ac0b1e65de3748cda130fef9a261a6f4bbe5ac24
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2020-05-18 23:47:20 -0500

    https

--- index.js
+++ index.js
@@ -3,6 +3,17 @@
 var mongoose = require('mongoose');
 var app = require('./app'); //importo el modulo app
 var port = 3900;
+var fs = require('fs');
+var https = require('https');
+
+var key = fs.readFileSync('encryption/server.key');
+var cert = fs.readFileSync( 'encryption/server.crt' );
+var options = {
+    key: key,
+    cert: cert
+  };
+
+var server = https.createServer(options, app);
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
@@ -12,7 +23,7 @@ mongoose.connect('mongodb://localhost:27017/vagos-db', { useNewUrlParser: true }
         console.log('buenos días');
 
         //crear servidor para escuchar peticionoes
-        app.listen(port, () => {
-            console.log('escuchando peticiones');
-        })
+        server.listen(port, () => {
+            console.log("server starting on port : " + port)
+        });
     });
