a9cafaaec042cf6bf69dad77ac1a31a377d9b6e4
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2020-06-09 01:04:00 -0500

    new changes

---
+++ VagosApi/assets/js/hoverEffect.js
@@ -0,0 +1,10 @@
+
+var hEfect= function(parentDiv){
+    new hoverEffect({
+    parent:parentDiv,
+    intensity:0.2,
+    image1: '../images/efect2.jpg',
+    image2: '../images/efect1.jpg',
+    displacementImage: '../images/heightMap.png'
+});
+}
