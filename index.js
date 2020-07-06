868b8cdc1073fe3ec0b16fea623e113c4606664b
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2020-07-05 19:41:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
 var mongoose = require('mongoose');
 var app = require('./app'); //importo el modulo app
 var port = 3900;
-var pott =3900;
 var fs = require('fs');
-//var https = require('https');
+var https = require('https');
 var http = require('http');
 
 
-/*const privateKey = fs.readFileSync('/etc/letsencrypt/live/vagosstudios.com/privkey.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/vagosstudios.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/vagosstudios.com/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/vagosstudios.com/chain.pem', 'utf8');
 
@@ -18,8 +17,8 @@ const options = {
 	cert: certificate,
 	ca: ca
 };
-var server = https.createServer(options, app);*/
-var serverHttp = http.createServer(app);
+var server = https.createServer(options, app);
+//http.createServer(app).listen(80);
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
@@ -29,16 +28,10 @@ mongoose.connect('mongodb://localhost:27017/vagos-db', { useNewUrlParser: true }
         console.log('buenos días');
 
         //crear servidor para escuchar peticionoes
-        /*server.listen(port, () => {
+        server.listen(port, () => {
             console.log("server starting on port : " + port);
         });
-        */
-        serverHttp.listen(pott, () => {
-            console.log("server starting on porto : " + pott);
-        });
-});
-
-
-http.get('*', function(req, res) {  
-    res.redirect('https://' + req.headers.host + req.url);
+        /*serverHttp.listen(port, () => {
+            console.log("server starting on port : " + port)
+        });*/
 });
