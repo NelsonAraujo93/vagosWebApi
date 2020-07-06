6298b693bd2492bcbcc8b377df783f4cd60d9d99
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2020-07-06 10:57:05 -0500

    changes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict'
 
+var express = require('express');
 var mongoose = require('mongoose');
 var app = require('./app'); //importo el modulo app
 var port = 3900;
+var pott = 8080;
 var fs = require('fs');
 var https = require('https');
 var http = require('http');
-
+const httpApp = express();
 
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/vagosstudios.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/vagosstudios.com/cert.pem', 'utf8');
@@ -18,7 +20,9 @@ const options = {
 	ca: ca
 };
 var server = https.createServer(options, app);
-//http.createServer(app).listen(80);
+
+httpApp.all('*', (req, res) => res.redirect(301, 'https://localhost:3900'));
+const httpServer = http.createServer(httpApp);
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
@@ -31,7 +35,7 @@ mongoose.connect('mongodb://localhost:27017/vagos-db', { useNewUrlParser: true }
         server.listen(port, () => {
             console.log("server starting on port : " + port);
         });
-        /*serverHttp.listen(port, () => {
-            console.log("server starting on port : " + port)
-        });*/
-});
+        httpServer.listen(pott, () => {
+            console.log("server starting on porto : " + pott);
+        });
+    });
