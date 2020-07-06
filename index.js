d65b342a43fa00d35d3611dd35bf2205e0ac0c01
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2020-07-05 19:33:38 -0500

    cambio

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose');
 var app = require('./app'); //importo el modulo app
 var port = 3900;
+var pott =3902;
 var fs = require('fs');
 var https = require('https');
 var http = require('http');
@@ -18,7 +19,7 @@ const options = {
 	ca: ca
 };
 var server = https.createServer(options, app);
-//http.createServer(app).listen(80);
+
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
@@ -34,4 +35,15 @@ mongoose.connect('mongodb://localhost:27017/vagos-db', { useNewUrlParser: true }
         /*serverHttp.listen(port, () => {
             console.log("server starting on port : " + port)
         });*/
-    });
+});
+
+var serverHttp = http.createServer(app);
+serverHttp.listen(pott, () => {
+    console.log("server starting on porto : " + pott);
+});
+http.get('*', function(req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+
+    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+    // res.redirect('https://example.com' + req.url);
+});
