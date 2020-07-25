f75e1929fd00565c6acfb340cdef78d903f0b498
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2020-07-25 17:28:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var appHttp = require('./appHttp');
 var port = 3900;
 var pott = 8080;
 var fs = require('fs');
-var https = require('https');
+//var https = require('https');
 var http = require('http');
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/vagosstudios.com/privkey.pem', 'utf8');
+/*const privateKey = fs.readFileSync('/etc/letsencrypt/live/vagosstudios.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/vagosstudios.com/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/vagosstudios.com/chain.pem', 'utf8');
 
@@ -17,8 +17,8 @@ const options = {
 	key: privateKey,
 	cert: certificate,
 	ca: ca
-};
-var server = https.createServer(options, app);
+};*/
+//var server = https.createServer(options, app);
 
 //appHttp.all('*', (req, res) => res.redirect(301, 'https://vagosstudios.com:3900'));
 const httpServer = http.createServer(appHttp);
